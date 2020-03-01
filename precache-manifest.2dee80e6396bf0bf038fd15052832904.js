a47269df20e64b55ae9b305d91b843286fc9abbd
Dennis Moore <denmoore93@gmail.com>
2020-03-01 12:30:50 -0600

    Updates

---
+++ precache-manifest.2dee80e6396bf0bf038fd15052832904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749981c13a13575343d62b228b804195",
+    "url": "/karlas-website/index.html"
+  },
+  {
+    "revision": "4ea98fd2adab513c5aa6",
+    "url": "/karlas-website/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "e0aad37862639c202b42",
+    "url": "/karlas-website/static/js/2.6a196c69.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/karlas-website/static/js/2.6a196c69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ea98fd2adab513c5aa6",
+    "url": "/karlas-website/static/js/main.37c07714.chunk.js"
+  },
+  {
+    "revision": "e0741401dc02000c1fca",
+    "url": "/karlas-website/static/js/runtime-main.bea712c2.js"
+  }
+]);
